e0fbd1484a976a5ad79070fd2e81790f7f3d8b80
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-02-27 21:45:52 -0300

    feature: adicionando algumas funções js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-// Chave API : a9849cffa9d900207666f41725a48ecd
+// Chave API
+const apiKey = "a9849cffa9d900207666f41725a48ecd";
+// URL da countryflags api
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+// Variávei de seleção de elementos
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+const cityElemnt = document.querySelector("#city");
+const tempElemnt = document.querySelector("#temperature span");
+const descElemnt = document.querySelector("#description");
+const weatherElemnt = document.querySelector("#weather-icon");
+const countryElemnt = document.querySelector("#country");
+const umidityElemnt = document.querySelector("#umidity span");
+const windElemnt = document.querySelector("#wind span");
+
+//Funções
+
+const getWeatherData = async(city) =>{
+	const apiWeatherURL = 
+}
+const showWeatherData = (city) =>{
+	console.log(city);
+}
+//Eventos
+
+/*Evento de clique do botão de buscar a cidade*/
+searchBtn.addEventListener("click", (e) =>{
+	e.preventDefault();
+
+	const city = cityInput.value;
+	
+	showWeatherData(city);
+
+})
